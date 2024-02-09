3a8a0358daa42ac798a3047921f41bc98446cbea
divko <test555_05@list.ru>
2024-02-09 14:52:11 +0300

    Finish

--- Program.cs
+++ Program.cs
@@ -10,16 +10,28 @@ namespace Task_2_1
     {
         static void Main(string[] args)
         {
+            // Task_2_1
             string fullName = "Иванов Иван Иванович";
             byte age = 30;
             string email = "Ivanov@mail.ru";
-            double ballsCode = 0;
-            double ballsMath = 0;
-            double ballsPhis = 0;
+            double ballsCode = 10;
+            double ballsMath = 20;
+            double ballsPhis = 30;
             string pattern = "ФИО: {0},\nВозраст: {1}, \nПочта: {2}, \nБаллы по программированию - {3}, \nБаллы по математике - {4}, \nБаллы по Физике - {5}.";
             Console.WriteLine(pattern,fullName,age,email,ballsCode,ballsMath,ballsPhis);
             Console.ReadKey();
-
+            // или интерполяция строк
+            Console.WriteLine($"ФИО: {fullName}" +
+                $",\n Возраст:{age}" +
+                $",\nПочта: {email}, \nБаллы по программированию - {ballsCode},\nБаллы по математике - {ballsMath},\nБаллы по физике -{ballsPhis}. ");
+            Console.ReadKey();
+            //Task 2_2
+            double nSum;
+            double nAverage;
+            nSum = (ballsCode + ballsMath + ballsPhis);
+            nAverage = nSum / 3;
+            Console.WriteLine($"Сумма баллов: {nSum}\nСреднее арифметическое: {nAverage}");
+            Console.ReadKey();
         }
     }
 }
