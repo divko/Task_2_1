0fe2da68c4e623af25c21b28f90367e71354f956
divko <test555_05@list.ru>
2024-02-09 14:32:42 +0300

    Writeline

--- Program.cs
+++ Program.cs
@@ -13,9 +13,12 @@ namespace Task_2_1
             string fullName = "Иванов Иван Иванович";
             byte age = 30;
             string email = "Ivanov@mail.ru";
-            double ballsCode;
-            double ballsMath;
-            double ballsPhis;
+            double ballsCode = 0;
+            double ballsMath = 0;
+            double ballsPhis = 0;
+            string pattern = "ФИО: {0},\nВозраст: {1}, \nПочта: {2}, \nБаллы по программированию - {3}, \nБаллы по математике - {4}, \nБаллы по Физике - {5}.";
+            Console.WriteLine(pattern,fullName,age,email,ballsCode,ballsMath,ballsPhis);
+            Console.ReadKey();
 
         }
     }
